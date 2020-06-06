09c558c82bc8d74ad9b8feee99824a0bafa97392
deku-M-O <1779881125@qq.com>
2020-06-06 09:41:27 +0800

    初始化

---
+++ run_game.py
@@ -0,0 +1,24 @@
+import pygame,sys
+import settings
+def run_game():
+    #初始化游戏
+    pygame.init()
+    #创建一个屏幕对象
+    ai_setting=Settings()
+    screen=pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
+    
+    #设置窗口名称
+    pygame.display.set_caption("唤兽世界")
+
+    while True:
+        #监视键盘与鼠标事件
+        for event in pygame.event.get():
+            #如果发生退出事件
+            if event.type==pygame.QUIT:
+                #系统退出
+                sys.exit()
+        pygame.display.flip()
+
+run_game()
+settings=Settings()
+print(settings.screen_width)
---
+++ settings.py
@@ -0,0 +1,4 @@
+class Setting():
+    def __init__(self):
+        self.screen_width=1200
+        self.screen_height=800
