73a76218e6ba523ed1dd2d945e93ed48ac0e68e1
deku-M-O <1779881125@qq.com>
2020-06-11 13:37:22 +0800

    增加上下左右

--- game_function.py
+++ game_function.py
@@ -1,4 +1,5 @@
 import pygame,sys
+
 def check_event(jack):
         #监视键盘与鼠标事件
     for event in pygame.event.get():
@@ -9,7 +10,13 @@ def check_event(jack):
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
                 jack.rect.centerx+=1
-        
+            elif event.key==pygame.K_LEFT:
+                jack.rect.centerx-=1
+            elif event.key==pygame.K_UP:
+                jack.rect.centery-=1
+            elif event.key==pygame.K_DOWN:
+                jack.rect.centery+=1
+
 def uodate_screen(screen,jack):
     screen.fill((112,233,143))
     jack.blitme()
