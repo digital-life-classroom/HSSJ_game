90879779ff021e0d0234bb4b0ab193a2fa1b0cb2
bbbbb <1779881125@qq.com>
2020-07-21 21:02:30 +0800

    调整字体，添加积分，未完成积分可变动

--- jifen.py
+++ jifen.py
@@ -2,10 +2,10 @@ import pygame
 class JiFen():
     def __init__(self):
         self.jifen=0
-        self.jiFen=pygame.font.SysFont("宋体",50)
-        self.jiFenRender=self.jiFen.render("abc",False,(255,170,0))
+        self.jiFen=pygame.font.SysFont("'simsunnsimsun",30)
+        self.jiFenRender=self.jiFen.render("杀敌数:"+str(self.jifen),False,(255,170,0))
         self.jiFenRect=self.jiFenRender.get_rect()
-        self.jiFenRect.left=1100
+        self.jiFenRect.left=1050
         self.jiFenRect.top=10
     def blit_me(self,screen):
         screen.blit(self.jiFenRender,self.jiFenRect)
